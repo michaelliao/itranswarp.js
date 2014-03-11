d9e7abd2f7c20847bc9985e09c9f41313fc784fb
Michael Liao <askxuefeng@gmail.com>
2014-03-11 22:20:41 +0800

    refactor api error

--- api.js
+++ api.js
@@ -9,6 +9,9 @@ function create_api_error(err_code, err_data, err_message) {
 }
 
 var api = {
+    invalid_param: function(paramName) {
+        return create_api_error('param:invalid', paramName, 'Invalid parameter: ' + paramName);
+    },
     not_allowed: function(err_message) {
         return create_api_error('permission:notallowed', '', err_message);
     },
@@ -16,6 +19,15 @@ var api = {
         return create_api_error('resource:notfound', err_data, err_message);
     },
     server_error: function(err_code, err_data, err_message) {
+        if (err_code instanceof Error) {
+            return create_api_error('', err_code.message, err_code.stack);
+        }
+        if (err_code && err_code.error) {
+            return err_code;
+        }
+        return create_api_error(err_code, err_data, err_message);
+    },
+    error: function(err_code, err_data, err_message) {
         if (err_code instanceof Error) {
             return create_api_error('', err_code.message, err_code.stack);
         }
