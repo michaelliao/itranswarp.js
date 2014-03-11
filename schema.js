8e977ce76508501f7a68e8f2f5b57bbf6b5631ea
Michael Liao <askxuefeng@gmail.com>
2014-03-11 22:28:38 +0800

    add test user

--- schema.js
+++ schema.js
@@ -26,5 +26,7 @@ _.each(keys.sort(), function(key, list) {
 });
 
 async.series(arr, function(err, results) {
-    console.log('\n--Done.');
+    console.log('\n-- init test user...\n');
+    console.log('\ninsert into t_user (id, role, name, email, passwd, verified, binds, image_url, locked_util, created_at, updated_at, version) values (\'00139454282989205c050f069414434a3588177998dcf73000\', 0, \'Admin\', \'admin@itranswarp.com\', \'e8f98b1676572cd24c753c331aa6b02e\', 1, \'\', \'http://about:blank\', 0, 1394542829892, 1394542829892, 0);');
+    console.log('\n--Done.\n');
 });
