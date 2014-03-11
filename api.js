132d5f0593e120da8959746f0a7d1bb86d05a4b8
Michael Liao <askxuefeng@gmail.com>
2014-03-11 18:16:05 +0800

    refactor api

--- api.js
+++ api.js
@@ -9,15 +9,18 @@ function create_api_error(err_code, err_data, err_message) {
 }
 
 var api = {
-    notallowed: function(err_data, err_message) {
-        return create_api_error('permission:notallowed', err_data, err_message);
+    not_allowed: function(err_message) {
+        return create_api_error('permission:notallowed', '', err_message);
     },
-    notfound: function(err_data, err_message) {
+    not_found: function(err_data, err_message) {
         return create_api_error('resource:notfound', err_data, err_message);
     },
-    error: function(err_code, err_data, err_message) {
+    server_error: function(err_code, err_data, err_message) {
         if (err_code instanceof Error) {
             return create_api_error('', err_code.message, err_code.stack);
+        }
+        if (err_code && err_code.error) {
+            return err_code;
         }
         return create_api_error(err_code, err_data, err_message);
     }
