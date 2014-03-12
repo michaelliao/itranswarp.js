8e68386a55093a641c5b40b3eb8275d551661281
Michael Liao <askxuefeng@gmail.com>
2014-03-12 18:12:35 +0800

    change underscore to lodash

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@
 
 var
     async = require('async'),
-    _ = require('underscore');
+    _ = require('lodash');
 
 var db = require('./db');
 
@@ -14,7 +14,7 @@ console.log('\n-- generating sqls...\n');
 
 var arr = [];
 
-_.each(keys.sort(), function(key, list) {
+_.each(keys.sort(), function(key) {
     if (key=='sequelize' || key=='next_id')
         return;
     arr.push(function(callback) {
@@ -25,8 +25,10 @@ _.each(keys.sort(), function(key, list) {
     });
 });
 
+var sql_init_user = 'insert into t_user (id, role, name, email, passwd, verified, binds, image_url, locked_util, created_at, updated_at, version) values (\'00139454282989205c050f069414434a3588177998dcf73000\', 0, \'Admin\', \'admin@itranswarp.com\', \'e8f98b1676572cd24c753c331aa6b02e\', 1, \'\', \'http://about:blank\', 0, 1394542829892, 1394542829892, 0)';
+
 async.series(arr, function(err, results) {
     console.log('\n-- init test user...\n');
-    console.log('\ninsert into t_user (id, role, name, email, passwd, verified, binds, image_url, locked_util, created_at, updated_at, version) values (\'00139454282989205c050f069414434a3588177998dcf73000\', 0, \'Admin\', \'admin@itranswarp.com\', \'e8f98b1676572cd24c753c331aa6b02e\', 1, \'\', \'http://about:blank\', 0, 1394542829892, 1394542829892, 0);');
+    console.log('\n' + sql_init_user + ';');
     console.log('\n--Done.\n');
 });
