a76638abf7a1943a81ab05629679a1825cab9f8b
Michael Liao <askxuefeng@gmail.com>
2014-03-12 18:13:26 +0800

    add APIError type

--- api.js
+++ api.js
@@ -1,41 +1,35 @@
 // api:
 
-function create_api_error(err_code, err_data, err_message) {
-    return {
-        error: err_code || 'internal:unknown',
-        data: err_data || '',
-        message: err_message || ''
-    };
+function APIError(err_code, err_data, err_message) {
+    this.error = err_code;
+    this.data = err_data;
+    this.message = err_message;
 }
 
 var api = {
     invalid_param: function(paramName) {
-        return create_api_error('param:invalid', paramName, 'Invalid parameter: ' + paramName);
+        return new APIError('parameter:invalid', paramName, 'Invalid parameter: ' + paramName);
     },
     not_allowed: function(err_message) {
-        return create_api_error('permission:notallowed', '', err_message);
+        return new APIError('permission:notallowed', '', err_message);
     },
     not_found: function(err_data, err_message) {
-        return create_api_error('resource:notfound', err_data, err_message);
+        return new APIError('resource:notfound', err_data, err_message);
     },
     server_error: function(err_code, err_data, err_message) {
         if (err_code instanceof Error) {
-            return create_api_error('', err_code.message, err_code.stack);
+            return new APIError('500', err_code.message, err_code.stack);
         }
         if (err_code && err_code.error) {
             return err_code;
         }
-        return create_api_error(err_code, err_data, err_message);
+        return new APIError(err_code, err_data, err_message);
     },
     error: function(err_code, err_data, err_message) {
-        if (err_code instanceof Error) {
-            return create_api_error('', err_code.message, err_code.stack);
-        }
-        if (err_code && err_code.error) {
-            return err_code;
-        }
-        return create_api_error(err_code, err_data, err_message);
-    }
+        console.log('create APIError: ' + err_code + ',' + err_data + ',' + err_message);
+        return new APIError(err_code, err_data, err_message);
+    },
+    APIError: APIError
 }
 
 exports = module.exports = api;
