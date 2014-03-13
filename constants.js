ec9aa9fb0d11abf44f75e4bcdf009ce4187434d0
Michael Liao <askxuefeng@gmail.com>
2014-03-13 18:36:54 +0800

    add constants

---
+++ constants.js
@@ -0,0 +1,13 @@
+// define constants:
+
+exports = module.exports = {
+    // user role:
+    ROLE_ADMIN:       0,
+    ROLE_EDITOR:      10,
+    ROLE_CONTRIBUTOR: 100,
+    ROLE_SUBSCRIBER:  10000,
+    ROLE_GUEST:       90000000,
+
+    // END:
+    END: 'ended.'
+};
