4ab978ed382458ea8f72f5326e2f5ad04e658f8f
Michael Liao <askxuefeng@gmail.com>
2014-03-16 22:31:01 +0800

    refactor utils

--- controllers/_utils.js
+++ controllers/_utils.js
@@ -19,6 +19,7 @@ var
 var SESSION_COOKIE_NAME = 'itranswarpsession';
 var salt = config.session.salt;
 
+// for safe base64 replacements:
 var
     re_add = new RegExp(/\+/g),
     re_sla = new RegExp(/\//g),
@@ -29,16 +30,82 @@ var
     re_r_sla = new RegExp(/\_/g),
     re_r_equ = new RegExp(/\./g);
 
+// string -> base64:
 function safe_b64encode(s) {
     var b64 = new Buffer(s).toString('base64');
     return b64.replace(re_add, '-').replace(re_sla, '_').replace(re_equ, '.');
 }
 
+// base64 -> string
 function safe_b64decode(s) {
     var b64 = s.replace(re_r_add, '+').replace(re_r_sla, '/').replace(re_r_equ, '=');
     return new Buffer(b64, 'base64').toString();
 }
 
+// Generate a secure client session cookie by constructing string:
+// base64(provider:uid:expires:md5(uid:expires:passwd:salt)).
+function make_session_cookie(provider, uid, passwd, expires) {
+    var now = Date.now();
+    var min = now + 86400000; // 1 day
+    var max = now + 2592000000; // 30 days
+    if (! expires) {
+        expires = now + 604800000; // default to 7 days;
+    }
+    else if (expires < min) {
+        expires = min;
+    }
+    else if (expires > max) {
+        expires = max;
+    }
+    var secure = [provider, uid, passwd, salt].join(':');
+    var md5 = crypto.createHash('md5').update(secure).digest('hex');
+    return safe_b64encode([provider, uid, expires, md5].join(':'));
+}
+
+// middle ware for extract session cookie,
+// and bind User to request object:
+function extract_session_cookie(req, res, next) {
+    req.user = null;
+    var cookie = req.cookies[SESSION_COOKIE_NAME];
+    if (cookie) {
+        return parse_session_cookie(cookie, function(err, user) {
+            if (err) {
+                return next(err);
+            }
+            if (user) {
+                user.passwd = '******';
+                req.user = user;
+                console.log('bind user from session cookie: ' + user.email);
+            }
+            else {
+                console.log('invalid session cookie. cleared.');
+                res.clearCookie(SESSION_COOKIE_NAME, {path: '/'});
+            }
+            return next();
+        });
+    }
+    console.log('no session cookie found.');
+    var auth = req.get('authorization');
+    if (auth) {
+        return parse_authorization(auth, function(err, user) {
+            if (err) {
+                return next(err);
+            }
+            if (user) {
+                user.passwd = '******'
+                req.user = user;
+                console.log('bind user from authorization: ' + user.email);
+            }
+            else {
+                console.log('invalid authorization header.');
+            }
+            return next();
+        });
+    }
+    return next();
+}
+
+// parse_session_cookie, with callback(err, user):
 function parse_session_cookie(s, fn) {
     var ss = safe_b64decode(s).split(':');
     if (ss.length != 4) {
@@ -67,6 +134,8 @@ function parse_session_cookie(s, fn) {
     });
 }
 
+// parse header 'Authorization: Basic xxxx',
+// with callback(err, user):
 function parse_authorization(auth, fn) {
     console.log('try parse header: Authorization: ' + auth);
     if ((auth.length < 6) || (auth.substring(0, 6)!=='Basic ')) {
@@ -95,25 +164,32 @@ function parse_authorization(auth, fn) {
     });
 }
 
+// ' A, B ; Ccc, ccc ' -> 'A,B,Ccc'
+function format_tags(tags) {
+    var arr = _.map(tags.split(/[\,\;]/), function(value) {
+        return value.trim();
+    });
+    var dict = {};
+    return _.filter(arr, function(value) {
+        if (value) {
+            var lv = value.toLowerCase();
+            if (lv in dict) {
+                return false;
+            }
+            dict[lv] = true;
+            return true;
+        }
+        return false;
+    }).join(',');
+}
+
 exports = module.exports = {
 
-    format_tags: function(tags) {
-        var arr = _.map(tags.split(/[\,\;]/), function(value) {
-            return value.trim();
-        });
-        var dict = {};
-        return _.filter(arr, function(value) {
-            if (value) {
-                var lv = value.toLowerCase();
-                if (lv in dict) {
-                    return false;
-                }
-                dict[lv] = true;
-                return true;
-            }
-            return false;
-        }).join(',');
-    },
+    format_tags: format_tags,
+
+    make_session_cookie: make_session_cookie,
+
+    extract_session_cookie: extract_session_cookie,
 
     // return parameter value as string, or default value if not exist. defaultValue is default to null.
     get_param: function(name, defaultValue, req) {
@@ -129,73 +205,6 @@ exports = module.exports = {
             return req.body[name].trim();
         }
         return null;
-    },
-
-    make_session_cookie: function(provider, uid, passwd, expires) {
-        /**
-            Generate a secure client session cookie by constructing string:
-            base64(provider:uid:expires:md5(uid:expires:passwd:salt)).
-        **/
-        var now = Date.now();
-        var min = now + 86400000; // 1 day
-        var max = now + 2592000000; // 30 days
-        if (! expires) {
-            expires = now + 604800000; // default to 7 days;
-        }
-        else if (expires < min) {
-            expires = min;
-        }
-        else if (expires > max) {
-            expires = max;
-        }
-        var secure = [provider, uid, passwd, salt].join(':');
-        var md5 = crypto.createHash('md5').update(secure).digest('hex');
-        return safe_b64encode([provider, uid, expires, md5].join(':'));
-    },
-
-    extract_session_cookie: function(req, res, next) {
-        req.user = null;
-        var cookie = req.cookies[SESSION_COOKIE_NAME];
-        if (cookie) {
-            return parse_session_cookie(cookie, function(err, user) {
-                if (err) {
-                    next(err);
-                }
-                else {
-                    if (user) {
-                        user.passwd = '******';
-                        req.user = user;
-                        console.log('bind user from session cookie: ' + user.email);
-                    }
-                    else {
-                        console.log('invalid session cookie. cleared.');
-                        res.clearCookie(SESSION_COOKIE_NAME, {path: '/'});
-                    }
-                    next();
-                }
-            });
-        }
-        console.log('no session cookie found.');
-        var auth = req.get('authorization');
-        if (auth) {
-            return parse_authorization(auth, function(err, user) {
-                if (err) {
-                    next(err);
-                }
-                else {
-                    if (user) {
-                        user.passwd = '******'
-                        req.user = user;
-                        console.log('bind user from authorization: ' + user.email);
-                    }
-                    else {
-                        console.log('invalid authorization header.');
-                    }
-                    next();
-                }
-            });
-        }
-        next();
     },
 
     findAll: function(Type, options, fn) {
