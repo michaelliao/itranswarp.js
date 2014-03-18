70b3e9becfcc8358e0825aa05b7c2ba2495d816b
Michael Liao <askxuefeng@gmail.com>
2014-03-18 18:10:48 +0800

    refactor attachments

--- controllers/attachments_api.js
+++ controllers/attachments_api.js
@@ -1,19 +1,47 @@
 // attachments.js
 
 var
-    async = require('async'),
+    fs = require('fs'),
+    mime = require('mime'),
     api = require('../api'),
     db = require('../db'),
     constants = require('../constants'),
+    dao = require('./_dao'),
     utils = require('./_utils');
+
+var
+    Canvas = require('canvas');
 
 var
     User = db.user,
     Attachment = db.attachment,
+    Resource = db.resource,
     sequelize = db.sequelize,
     next_id = db.next_id;
 
+function atta_file(req, res, next, prev) {
+    utils.find(Attachment, req.params.id, function(err, atta) {
+        if (err) {
+            return next(err);
+        }
+        var mime = atta.mime,
+            data = atta.data;
+        if (prev==='s') {
+            // generate small image: 160 x N
+        }
+        else if (prev==='m') {
+            // generate medium image: 640 x N
+        }
+        res.type(mime);
+        res.send(data);
+    });
+}
+
 exports = module.exports = {
+
+    'GET /files/attachments/:id': atta_file,
+
+    'GET /files/attachments/:id/0': atta_file,
 
     'POST /api/attachments': function(req, res, next) {
         /**
@@ -21,68 +49,57 @@ exports = module.exports = {
          * 
          * @return {object} The created attachment object.
          */
-        if ( ! req.user || req.user.role > ROLE_CONTRIBUTOR) {
-            return res.send(api.notallowed('Permission denied.'));
+        //if (utils.isForbidden(req, constants.ROLE_CONTRIBUTOR)) {
+        //    return next(api.not_allowed('Permission denied.'));
+        //}
+        var user_id = 'xxx';
+
+        try {
+            var name = utils.get_required_param('name', req);
         }
+        catch (e) {
+            return next(e);
+        }
+        var description = utils.get_param('description', '', req);
 
-        var name = req.body.name.trim();
-        var description = req.body.description.trim();
+        var file = req.files.file;
+        var size = file.size,
+            type = mime.lookup(file.originalFilename);
 
-        var category_id = req.body.category_id;
+        console.log('file uploaded: ' + file.name);
 
-        var content = req.body.content;
-        var tags = req.body.tags;
-        var publish_time = Date.now(); //req.body.publish_time;
-
-        var cover_id = '';
-        var article_id = next_id();
-
-        var tasks = {
-            category: function(callback) {
-                utils.get_category(category_id, function(obj) {
-                    callback(null, obj);
-                });
-            },
-            text: function(callback) {
-                utils.create_object(Text, { ref_id: article_id, value: content }, tx, function(err, obj) {
-                    callback(err, obj);
-                });
-            },
-            article: function(callback) {
-                utils.create_object(Article, {
-                    id: article_id,
-                    user_id: req.user.id,
-                    category_id: category_id,
-                    cover_id: cover_id,
-                    name: name,
-                    description: description,
-                    publish_time: publish_time
-                }, tx, function(err, obj) {
-                    callback(err, obj);
-                });
+        fs.readFile(file.path, function(err, fcontent) {
+            if (err) {
+                return next(err);
             }
-        };
-        if (cover_id) {
-            // TODO: add create cover task:
-        }
-        sequelize.transaction(function(tx) {
-            async.series([
-                function(callback) {
-                    //
-                }
-            ], function(err, results) {
+            // for image type, check width & height:
+            var width = 0,
+                height = 0;
+            if (type.indexOf('image/')===0) {
+                var img = new Canvas.Image;
+                img.src = fcontent;
+                width = img.width;
+                height = img.height;
+                console.log('check image size: ' + width + ' x ' + height);
+            }
+            dao.save(Attachment, {
+                user_id: user_id,
+                size: size,
+                mime: type,
+                meta: '',
+                width: width,
+                height: height,
+                name: name,
+                description: description,
+                data: fcontent
+            }, function(err, atta) {
                 if (err) {
-                    return tx.rollback().success(function() {
-                        return next(err);
-                    });
+                    return next(err);
                 }
-                tx.commit().error(function(err) {
-                    return res.send(api.server_error(err));
-                }).success(function() {
-                    return res.send(results.article);
-                });
+                atta.dataValues.data = '[binary data]';
+                return res.send(atta);
             });
         });
-    },
+    }
 
 }
--- models/attachment.js
+++ models/attachment.js
@@ -5,10 +5,13 @@ var Base = require('./_base.js');
 exports = module.exports = function(sequelize, DataTypes) {
     return Base.create(sequelize, DataTypes, 'Attachment', {
         user_id: Base.column_id(),
-        resource_id: Base.column_id(),
         size: Base.column_bigint(),
-        kind: Base.column_varchar_100(),
-        name: Base.column_varchar_100(),
-        description: Base.column_varchar_500()
+        width: Base.column_bigint(),
+        height: Base.column_bigint(),
+        mime: Base.column_varchar_100(),
+        name: Base.column_name(),
+        meta: Base.column_varchar_100(),
+        description: Base.column_varchar_500(),
+        data: Base.column_blob()
     });
 }
