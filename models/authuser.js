d08393a2a42bd09d1b216b9ba327f4367a4e6084
Michael Liao <askxuefeng@gmail.com>
2014-04-03 18:19:06 +0800

    change auth_token to varchar(500)

--- models/authuser.js
+++ models/authuser.js
@@ -7,7 +7,7 @@ exports = module.exports = function(warp) {
         base.column_id('user_id'),
         base.column_varchar_50('auth_provider'),
         base.column_varchar_100('auth_id', { unique: true }),
-        base.column_varchar_100('auth_token'),
+        base.column_varchar_500('auth_token'),
         base.column_bigint('expires_time')
     ], {
         table: 'auth_users'
