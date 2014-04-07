d28c251a9607c020d71411582eff05bba4a92717
Michael Liao <askxuefeng@gmail.com>
2014-04-07 22:24:24 +0800

    fix name typo

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@
 var base = require('./_base.js');
 
 exports = module.exports = function(warp) {
-    return base.defineModel(warp, 'Comment', [
+    return base.defineModel(warp, 'Category', [
         base.column_varchar_100('name'),
         base.column_varchar_1000('description'),
         base.column_bigint('display_order')
