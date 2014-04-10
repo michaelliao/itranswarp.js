0ee7d7156918f4fb5498f7eb6b9d37697f8a371f
Michael Liao <askxuefeng@gmail.com>
2014-04-10 17:57:44 +0800

    generate admin email, password by input

--- schema.js
+++ schema.js
@@ -2,6 +2,8 @@
 
 var
     _ = require('lodash'),
+    crypto = require('crypto'),
+    readline = require('readline'),
     db = require('./db');
 
 var keys = _.filter(_.map(db, function(value, key) {
@@ -15,14 +17,33 @@ function log(s) {
     console.log('\n');
 }
 
-log('-- generating ddl...');
+function generateDDL(email, password) {
+    log('-- generating ddl...');
 
-_.each(keys.sort(), function(key) {
-    log('-- generate model: ' + key);
-    log(db[key].ddl());
+    _.each(keys.sort(), function(key) {
+        log('-- generate model: ' + key);
+        log(db[key].ddl());
+    });
+
+    log('-- create administrator:\n-- Email: ' + email + '\n-- Password: ' + new Array(password.length).join('*'));
+    var
+        id = db.next_id(),
+        passwd = crypto.createHash('md5').update(email + ':' + password).digest('hex');
+    var sql_init_admin_user = 'insert into users (id, role, name, email, passwd, verified, image_url, locked_util, created_at, updated_at, version) values (\'' + id + '\', 0, \'Admin\', \'' + email + '\', \'' + passwd + '\', 1, \'http://about:blank\', 0, 1394002009000, 1394002009000, 0);';
+    log(sql_init_admin_user);
+    log('-- done.');
+}
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-log('-- init admin user...');
-var sql_init_admin_user = 'insert into users (id, role, name, email, passwd, verified, image_url, locked_util, created_at, updated_at, version) values (\'00139454282989205c050f069414434a3588177998dcf73000\', 0, \'Admin\', \'admin@itranswarp.com\', \'e8f98b1676572cd24c753c331aa6b02e\', 1, \'http://about:blank\', 0, 1394002009000, 1394002009000, 0);';
-log(sql_init_admin_user);
-log('-- done.');
+log('Create administrator account:');
+
+rl.question('Email: ', function(email) {
+    rl.question('Password: ', function(password) {
+        rl.close();
+        generateDDL(email.trim().toLowerCase(), password);
+    });
+});
