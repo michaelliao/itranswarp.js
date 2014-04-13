dfc42816f473da5be5381fdc973128a24931cabc
Michael Liao <askxuefeng@gmail.com>
2014-04-13 20:30:28 +0800

    add a connection timeout

--- config_default.js
+++ config_default.js
@@ -1,17 +1,41 @@
+/*
+ * This is the default configuration for iTranswarp.js.
+ * 
+ * DO NOT change it. Instead, make a copy and name to "config_override.js",
+ * then edit the settings.
+ */
 exports = module.exports = {
-    "session": {
-        "salt": "iTranswarp.js"
+    'session': {
+        // used to generate secure session cookie, can be set to any random string:
+        'salt': 'iTranswarp.js'
     },
-    "db": {
-        "host": "localhost",
-        "port": 3306,
-        "user": "www",
-        "password": "www",
-        "database": "itranswarp",
-        "connectionLimit": 20
+    'db': {
+        // host or ip address of mysql, e.g. '192.168.1.123':
+        'host': 'localhost',
+        // port of mysql, default to 3306:
+        'port': 3306,
+        // user to login to mysql, change to your mysql user:
+        'user': 'www',
+        // password to login to mysql, change to your mysql password:
+        'password': 'www',
+        // database used in mysql, default to 'itranswarp':
+        'database': 'itranswarp',
+        // timeout before initial a connection to mysql, default to 3 seconds:
+        'connectTimeout': 3000,
+        // maximum concurrent db connections:
+        'connectionLimit': 20
     },
-    "domain": "www.example.com",
-    "theme": "default",
-    "oauth2": {
+    // server domain name:
+    'domain': 'www.example.com',
+    // the theme used, default to 'default':
+    'theme': 'default',
+    // oauth2 providers that allow sign in from other oauth2 providers:
+    'oauth2': {
+        // e.g. facebook oauth2 configuration:
+        // 'faceook': {
+        //     'app_key': 'your-app-id',
+        //     'app_secret': 'your-app-secret',
+        //     'redirect_uri': 'http://your-redirect-uri/config/in/facebook'
+        // }
     }
 }
