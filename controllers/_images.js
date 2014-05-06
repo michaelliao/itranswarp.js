a7c877a403a298e54141175e64b740b83898cbba
Michael Liao <askxuefeng@gmail.com>
2014-05-06 14:45:27 +0800

    refactor to pass jslint

--- controllers/_images.js
+++ controllers/_images.js
@@ -19,6 +19,7 @@ function calcScaleSize(origin_width, origin_height, resize_width, resize_height,
         return { width: origin_width, height: origin_height, resized: false, enlarge: false };
     }
     var
+        expected_height,
         target_width = resize_width,
         target_height = resize_height;
     if (target_height <= 0) {
@@ -30,22 +31,21 @@ function calcScaleSize(origin_width, origin_height, resize_width, resize_height,
         return { width: target_width, height: target_height, resized: true, enlarge: isEnlarge(target_width, target_height) };
     }
     if (keepAspect) {
-        var expected_height = target_width * origin_height / origin_width;
+        expected_height = target_width * origin_height / origin_width;
         if (expected_height > target_height) {
             target_width = target_height * origin_width / origin_height;
-        }
-        else if (expected_height < target_height) {
+        } else if (expected_height < target_height) {
             target_height = expected_height;
         }
     }
     return { width: target_width, height: target_height, resized: true, enlarge: isEnlarge(target_width, target_height) };
 }
 
-exports = module.exports = {
+module.exports = {
 
     calcScaleSize: calcScaleSize,
 
-    getSize: function(imgData, callback) {
+    getSize: function (imgData, callback) {
         gm(imgData).size(callback);
     },
 
@@ -54,17 +54,17 @@ exports = module.exports = {
      * if options.stream is true, callback should have signature (err, stdout, stderr), otherwise,
      * callback(err, buffer).
      */
-    resize: function(imgData, origin_width, origin_height, resize_width, resize_height, options, callback) {
-        if (typeof(options)==='function') {
+    resize: function (imgData, origin_width, origin_height, resize_width, resize_height, options, callback) {
+        if (typeof options === 'function') {
             callback = options;
             options = {};
         }
         options = options || {};
         var
-            opt_force = options.force===undefined ? false : true,
-            opt_stream = options.stream===undefined ? true : false;
-        var img = gm(imgData);
-        var r = calcScaleSize(origin_width, origin_height, resize_width, resize_height, options.keepAspect || true);
+            opt_force = options.force === undefined ? false : true,
+            opt_stream = options.stream === undefined ? true : false,
+            img = gm(imgData),
+            r = calcScaleSize(origin_width, origin_height, resize_width, resize_height, options.keepAspect || true);
         if (r.resized && (opt_force || !r.enlarge)) {
             console.log('resized to ' + r.width + 'x' + r.height);
             img = img.resize(r.width, r.height);
@@ -74,6 +74,6 @@ exports = module.exports = {
         }
         return img.toBuffer(callback);
     }
-}
+};
 
 calcScaleSize(1200, 1200, 800, 600);
