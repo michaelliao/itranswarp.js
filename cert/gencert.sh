6f71356cb2d5fc3a26deadf4ece84b7a50560a68
Michael Liao <askxuefeng@gmail.com>
2014-05-06 09:44:35 +0800

    update gencert script

--- cert/gencert.sh
+++ cert/gencert.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 
-# create self-signed certs
+# create self-signed server certificate:
 
-read -p "Enter your domain: " DOMAIN
+read -p "Enter your domain [www.example.com]: " DOMAIN
 
 echo "Create server key..."
 
-openssl genrsa -des3 -out server.key 1024
+openssl genrsa -des3 -out $DOMAIN.key 1024
 
 echo "Create server certificate signing request..."
 
 SUBJECT="/C=US/ST=Mars/L=iTranswarp/O=iTranswarp/OU=iTranswarp/CN=$DOMAIN"
 
-openssl req -new -subj $SUBJECT -key server.key -out server.csr
+openssl req -new -subj $SUBJECT -key $DOMAIN.key -out $DOMAIN.csr
 
 echo "Remove password..."
 
-mv server.key server.key.org
-openssl rsa -in server.key.org -out server.key
+mv $DOMAIN.key $DOMAIN.origin.key
+openssl rsa -in $DOMAIN.origin.key -out $DOMAIN.key
 
 echo "Sign SSL certificate..."
 
-openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -days 3650 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
 
 echo "TOTO:"
-echo "Copy server.crt to /etc/nginx/ssl/server.crt"
-echo "Copy server.key to /etc/nginx/ssl/server.key"
+echo "Copy $DOMAIN.crt to /etc/nginx/ssl/$DOMAIN.crt"
+echo "Copy $DOMAIN.key to /etc/nginx/ssl/$DOMAIN.key"
 echo "Add configuration in nginx:"
 echo "server {"
 echo "    ..."
 echo "    ssl on;"
-echo "    ssl_certificate     /etc/nginx/ssl/server.crt;"
-echo "    ssl_certificate_key /etc/nginx/ssl/server.key;"
+echo "    ssl_certificate     /etc/nginx/ssl/$DOMAIN.crt;"
+echo "    ssl_certificate_key /etc/nginx/ssl/$DOMAIN.key;"
 echo "}"
