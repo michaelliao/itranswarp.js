31b5659b989edb08305b1ca49b3aa3b50d690d4a
Michael Liao <askxuefeng@gmail.com>
2014-08-18 18:58:47 +0800

    add score field for scoring

--- models/reply.js
+++ models/reply.js
@@ -10,6 +10,7 @@ module.exports = function (warp) {
         base.column_boolean('deleted'),
         base.column_bigint('upvotes'),
         base.column_bigint('downvotes'),
+        base.column_bigint('score'),
         base.column_text('content', { type: 'text' })
     ], {
         table: 'replies'
--- models/topic.js
+++ models/topic.js
@@ -11,6 +11,7 @@ module.exports = function (warp) {
         base.column_varchar_1000('tags'),
         base.column_bigint('upvotes'),
         base.column_bigint('downvotes'),
+        base.column_bigint('score'),
         base.column_boolean('locked'),
         base.column_boolean('deleted'),
         base.column_text('content', { type: 'text' })
