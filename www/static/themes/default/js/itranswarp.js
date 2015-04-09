70423bb95dbb46a1190c3ebbb6be6b3cf706ee7c
Michael Liao <askxuefeng@gmail.com>
2015-04-09 22:57:24 +0800

    update js

--- www/static/themes/default/js/itranswarp.js
+++ www/static/themes/default/js/itranswarp.js
@@ -1,1 +1,159 @@
 // itranswarp.js
+
+if (! window.console) {
+    window.console = {
+        log: function(s) {
+        }
+    };
+}
+
+if (! String.prototype.trim) {
+    String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+    };
+}
+
+if (! Number.prototype.toDateTime) {
+    var replaces = {
+        'yyyy': function(dt) {
+            return dt.getFullYear().toString();
+        },
+        'yy': function(dt) {
+            return (dt.getFullYear() % 100).toString();
+        },
+        'MM': function(dt) {
+            var m = dt.getMonth() + 1;
+            return m < 10 ? '0' + m : m.toString();
+        },
+        'M': function(dt) {
+            var m = dt.getMonth() + 1;
+            return m.toString();
+        },
+        'dd': function(dt) {
+            var d = dt.getDate();
+            return d < 10 ? '0' + d : d.toString();
+        },
+        'd': function(dt) {
+            var d = dt.getDate();
+            return d.toString();
+        },
+        'hh': function(dt) {
+            var h = dt.getHours();
+            return h < 10 ? '0' + h : h.toString();
+        },
+        'h': function(dt) {
+            var h = dt.getHours();
+            return h.toString();
+        },
+        'mm': function(dt) {
+            var m = dt.getMinutes();
+            return m < 10 ? '0' + m : m.toString();
+        },
+        'm': function(dt) {
+            var m = dt.getMinutes();
+            return m.toString();
+        },
+        'ss': function(dt) {
+            var s = dt.getSeconds();
+            return s < 10 ? '0' + s : s.toString();
+        },
+        's': function(dt) {
+            var s = dt.getSeconds();
+            return s.toString();
+        },
+        'a': function(dt) {
+            var h = dt.getHours();
+            return h < 12 ? 'AM' : 'PM';
+        }
+    };
+    var token = /([a-zA-Z]+)/;
+    Number.prototype.toDateTime = function(format) {
+        var fmt = format || 'yyyy-MM-dd hh:mm:ss'
+        var dt = new Date(this);
+        var arr = fmt.split(token);
+        for (var i=0; i<arr.length; i++) {
+            var s = arr[i];
+            if (s && s in replaces) {
+                arr[i] = replaces[s](dt);
+            }
+        }
+        return arr.join('');
+    };
+}
+
+function toSmartDate(timestamp) {
+    if (typeof(timestamp)==='string') {
+        timestamp = parseInt(timestamp);
+    }
+    if (isNaN(timestamp)) {
+        return '';
+    }
+    var
+        today = new Date(g_time),
+        now = today.getTime(),
+        s = '1分钟前',
+        t = now - timestamp;
+    if (t > 604800000) {
+        // 1 week ago:
+        var that = new Date(timestamp);
+        var
+            y = that.getFullYear(),
+            m = that.getMonth() + 1,
+            d = that.getDate(),
+            hh = that.getHours(),
+            mm = that.getMinutes();
+        s = y===today.getFullYear() ? '' : y + '-';
+        s = s + m + '-' + d + ' ' + hh + ':' + (mm < 10 ? '0' : '') + mm;
+    }
+    else if (t >= 86400000) {
+        // 1-6 days ago:
+        s = Math.floor(t / 86400000) + '天前';
+    }
+    else if (t >= 3600000) {
+        // 1-23 hours ago:
+        s = Math.floor(t / 3600000) + '小时前';
+    }
+    else if (t >= 60000) {
+        s = Math.floor(t / 60000) + '分钟前';
+    }
+    return s;
+}
+
+$(function() {
+    // init scroll:
+    var $window = $(window);
+    var $body = $('body');
+    var $gotoTop = $('div.x-goto-top');
+    $window.scroll(function() {
+        var t = $window.scrollTop();
+        if (t > 1600) {
+            $gotoTop.show();
+        }
+        else {
+            $gotoTop.hide();
+        }
+    });
+
+    // go-top:
+    $gotoTop.click(function() {
+        $('html, body').animate({scrollTop: 0}, 1000);
+    });
+
+    // smart date:
+    $('.x-smartdate').each(function() {
+        var f = parseInt($(this).attr('date'));
+        $(this).text(toSmartDate(f));
+    });
+
+    // search query:
+    // var input_search = $('input.search-query');
+    // var old_width = input_search.css('width');
+    // input_search.bind('focusin', function() {
+    //     input_search.animate({'width': '160px'}, 500);
+    // }).bind('focusout', function() {
+    //     input_search.animate({'width': old_width}, 500);
+    // });
+
+    hljs.initHighlighting();
+    // END
+});
