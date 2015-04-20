632835e659696a716a7db94bdb100a9aedf418e9
Michael Liao <askxuefeng@gmail.com>
2015-04-20 16:46:06 +0800

    update gulpfile

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,12 +1,48 @@
 var
-    jslint = require('gulp-jslint'),
+    _ = require('lodash'),
+    fs = require('fs'),
     less = require('gulp-less'),
+    jslint = require('gulp-jslint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
     gulp = require('gulp');
+
+var theme = 'default';
+
+function getJavaScriptFiles(file) {
+    var
+        re = /^.*\<script\s+src\=\"(.*)\"\>.*$/,
+        data = fs.readFileSync(file, { encoding: 'utf-8' }),
+        begin = data.indexOf('<!-- BEGIN JAVASCRIPT COMPRESS -->'),
+        end = data.indexOf('<!-- END JAVASCRIPT COMPRESS -->'),
+        lines;
+    if (begin === (-1) || end === (-1) || begin > end) {
+        throw 'Error: special comment not found!';
+    }
+    lines = data.substring(begin, end).split('\n');
+    lines = _.map(lines, function (line) {
+        var m = re.exec(line);
+        if (m) {
+            return m[1].replace(/\{\{\s*\_\_theme\_\_\s*\}\}/, theme);
+        }
+        return null;
+    });
+    lines = _.filter(lines, function (line) {
+        return line !== null;
+    });
+    return _.map(lines, function (line) {
+        return '.' + line;
+    });
+}
+
+console.log(getJavaScriptFiles('./views/themes/' + theme + '/_base.html'));
 
 gulp.task('jslint', function () {
     return gulp.src([
         './controllers/*.js',
         './models/*.js',
+        './search/*.js',
         './*.js'
     ]).pipe(jslint({
         node: true,
@@ -18,12 +54,18 @@ gulp.task('jslint', function () {
     }));
 });
 
+gulp.task('uglify', function () {
+    var jsfiles = getJavaScriptFiles('./views/themes/' + theme + '/_base.html');
+    return gulp.src(jsfiles)
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./static/js'));
+});
+
 gulp.task('less', function () {
-    return gulp.src([
-        './static/css/itranswarp.css.less/itranswarp-*.less'
-    ]).pipe(less({
-        paths: ['./static/css/itranswarp.css.less']
-    })).pipe(gulp.dest('./static/css'));
+    return gulp.src(['./static/css/itranswarp.less'])
+        .pipe(less())
+        .pipe(gulp.dest('./static/css'));
 });
 
 gulp.task('default', ['jslint', 'less']);
