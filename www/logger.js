152276f4144bbd4e1146588ed005ea49fa6c9207
Michael Liao <askxuefeng@gmail.com>
2017-03-09 11:46:07 +0800

    minor refactor

--- www/logger.js
+++ www/logger.js
@@ -8,35 +8,34 @@
  */
 const
     winston = require('winston'),
-    config = require('./config');
-
-var logger = new winston.Logger({
-    transports: [
-        new winston.transports.Console({
-            name: 'console',
-            colorize: true,
-            level: 'info',
-            timestamp: true
-        }),
-        new winston.transports.File({
-            name: 'info-file',
-            filename: config.log.dir + '/itranswarp-info.log',
-            level: 'info',
-            json: false,
-            timestamp: function () {
-                return new Date().toString();
-            }
-        }),
-        new winston.transports.File({
-            name: 'error-file',
-            filename: config.log.dir + '/itranswarp-error.log',
-            level: 'error',
-            json: false,
-            timestamp: function () {
-                return new Date().toString();
-            }
-        })
-    ]
-});
+    config = require('./config'),
+    logger = new winston.Logger({
+        transports: [
+            new winston.transports.Console({
+                name: 'console',
+                colorize: true,
+                level: 'info',
+                timestamp: true
+            }),
+            new winston.transports.File({
+                name: 'info-file',
+                filename: config.log.dir + '/itranswarp-info.log',
+                level: 'info',
+                json: false,
+                timestamp: function () {
+                    return new Date().toString();
+                }
+            }),
+            new winston.transports.File({
+                name: 'error-file',
+                filename: config.log.dir + '/itranswarp-error.log',
+                level: 'error',
+                json: false,
+                timestamp: function () {
+                    return new Date().toString();
+                }
+            })
+        ]
+    });
 
 module.exports = logger;
